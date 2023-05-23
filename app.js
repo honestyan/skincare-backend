71395ae00146b27a6e141050f96a2617173f4430
Honestyan <honestyan0708@gmail.com>
2023-05-23 20:52:07 +0700

    test: env

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
 });
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = server;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+require("dotenv").config();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 // const auth = require("./auth");
@@ -7,7 +8,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   return res.status(200).json({
     status: true,
-    message: "Welcome to Skincare API",
+    message: `Welcome to Skincare API. ${process.env._TEST_ENV}`,
   });
 });
 
