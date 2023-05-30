59f91773531ad2424d8c8c013c515357141ff2e8
Honestyan <honestyan0708@gmail.com>
2023-05-30 18:43:10 +0700

    add: flow logic product crud

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const auth = require("./auth");
 const upload = require("./upload");
 const userSkin = require("./userSkin");
+const product = require("./product");
 
 module.exports = {
   auth,
   upload,
   userSkin,
+  product,
 };
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const { WELCOME_MESSAGE } = process.env;
 const auth = require("./auth.js");
 const upload = require("./upload.js");
 const userSkin = require("./userSkin.js");
+const product = require("./product.js");
 
 router.get("/", (req, res) => {
   return res.status(200).json({
@@ -18,5 +19,6 @@ router.get("/", (req, res) => {
 router.use("/auth", auth);
 router.use("/upload", upload);
 router.use("/userSkin", userSkin);
+router.use("/product", product);
 
 module.exports = router;
